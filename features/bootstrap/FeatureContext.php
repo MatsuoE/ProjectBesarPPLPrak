cb7f41890226f7583be4a4bacfa81c789f35f322
Auliya <liet60@gmail.com>
2023-04-04 03:51:24 +0700

    testing bdd behat

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -3,12 +3,14 @@
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
-
+use Behat\Behat\Tester\Exception\PendingException;
+use GuzzleHttp\Client;
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext implements Context
 {
+    protected $response;
     /**
      * Initializes context.
      *
@@ -19,4 +21,68 @@ class FeatureContext implements Context
     public function __construct()
     {
     }
+
+    /**
+     * @Given the user is on the main dashboard
+     */
+    public function theUserIsOnTheMainDashboard()
+    {
+        $client = new Client(['base_uri' => 'http://localhost:8000']);
+        $response = $client->get(uri:'/');
+        $responseCode = $response->getStatusCode();
+        if ($responseCode != 200){
+            throw new Exception("Error Processing Request");
+        }
+        return true;
+    }
+
+    /**
+     * @When the user go to Susunan MK page
+     */
+    public function theUserGoToSusunanMkPage()
+    {
+        $client = new Client(['base_uri' => 'http://localhost:8000/susunanmk']);
+        $this->response = $client->get(uri:'/susunanmk');
+        $responseCode = $this->response->getStatusCode();
+        if ($responseCode != 200){
+            throw new Exception("Error Processing Request");
+        }
+        return true;
+    }
+
+    /**
+     * @Then the user should see MK with ID :arg1
+     */
+    public function theUserShouldSeeMkWithId($arg1)
+{
+    // Connect to the database
+    $host = '127.0.0.1';
+    $db   = 'projectbesarppl';
+    $user = 'root';
+    $pass = '';
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    $pdo = new PDO($dsn, $user, $pass, $options);
+
+    // Fetch data from the "susunanmk" table
+    $stmt = $pdo->query('SELECT * FROM susunanmk');
+    $mk = $stmt->fetchAll();
+
+    // Check if the MK with ID $arg1 exists
+    foreach ($mk as $matkul) {
+        if ($matkul['kodeMK'] == $arg1) {
+            return true;
+        }
+    }
+
+    // MK with ID $arg1 not found, throw an exception
+    throw new Exception("Error Processing Request");
+}
+
 }
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', [susunanMKController::class, 'index']);
+Route::get('/susunanmk', [susunanMKController::class, 'index']);
 Route::get('/organisasimk', [organisasiMKController::class, 'index']);
 Route::resource('susunanMK', susunanMKController::class);
